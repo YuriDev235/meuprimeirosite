f157319a2a0668c7b0ace90486f397bea9f487b6
Miojo123 <yurigabrielgoncalvesribeiro@gmail.com>
2026-07-12 20:40:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const botao = document.getElementById("gerar");
+const download = document.getElementById("download");
 const input = document.getElementById("link");
 const imagem = document.getElementById("qrcode");
-botao.addEventListener("click", function(){
+
+botao.addEventListener("click", function () {
 
     const link = input.value;
 
+    if (link === "") {
+        alert("Digite um link!");
+        return;
+    }
+
     imagem.src = "https://api.qrserver.com/v1/create-qr-code/?size=220x220&data=" + encodeURIComponent(link);
 
-})
-if(link.trim() === ""){
-    alert("Digite um link primeiro!");
-    return;
-}
+    imagem.onload = function () {
+        download.style.display = "block";
+    };
+
+});
